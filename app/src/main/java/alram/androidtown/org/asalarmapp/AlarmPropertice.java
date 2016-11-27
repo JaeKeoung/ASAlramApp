7cd053ef1ae16b6af01306a24b6b050c9fc32ab6
JaeKeoung <tkdlgks@naver.com>
2016-11-27 12:45:17 +0900

    161127

---
+++ app/src/main/java/alram/androidtown/org/asalarmapp/AlarmPropertice.java
@@ -0,0 +1,58 @@
+package alram.androidtown.org.asalarmapp;
+
+import android.app.Activity;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class AlarmPropertice extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarm_propertice);
+
+        SeekBar seekVolume = (SeekBar) findViewById(R.id.volumeBar);
+            final AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+            int nMax = audioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM);
+            int nCurrentVolume = audioManager.getStreamVolume(AudioManager.STREAM_ALARM);
+
+            seekVolume.setMax(nMax);
+            seekVolume.setProgress(nCurrentVolume);
+
+            seekVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    audioManager.setStreamVolume(AudioManager.STREAM_ALARM, progress, 0);
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+
+        Button cancelBtn = (Button) findViewById(R.id.cancel_btn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        Button saveBtn = (Button) findViewById(R.id.cancel_btn);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}
--- app/src/main/java/alram/androidtown/org/asalarmapp/MainActivity.java
+++ app/src/main/java/alram/androidtown/org/asalarmapp/MainActivity.java
@@ -1,12 +1,27 @@
 package alram.androidtown.org.asalarmapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarm_main);
+
+        Button addAlarm = (Button) findViewById(R.id.addA);
+        addAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callAlarmPropertice = new Intent(v.getContext(), AlarmPropertice.class);
+                startActivity(callAlarmPropertice);
+
+            }
+        });
+
     }
 }
