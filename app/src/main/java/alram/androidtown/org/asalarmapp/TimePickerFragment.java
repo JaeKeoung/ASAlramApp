6a66028091d7e357bece8d9c3d83ff0736e276e0
JonghunYu <yuzong@naver.com>
2016-12-04 12:49:25 +0900

    get rid of red lines

--- app/src/main/java/alram/androidtown/org/asalarmapp/TimePickerFragment.java
+++ app/src/main/java/alram/androidtown/org/asalarmapp/TimePickerFragment.java
@@ -3,21 +3,26 @@ package alram.androidtown.org.asalarmapp;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
-import android.icu.text.DateFormat;
-import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.widget.TimePicker;
 
-public static class TimePickerFragment extends DialogFragment {
+import android.text.format.DateFormat;
+import java.util.Calendar;
+
+public class TimePickerFragment extends DialogFragment
     implements TimePickerDialog.OnTimeSetListener {
-        @Override
-        public Dialog onCreateDialog(Bundle savedlnstanceState) {
-            //현재 시간을 picker의 기본 값으로 사용합니다.
-            final Calendar c = Calendar.getInstance();
-            int hour = c.get(Calendar.HOUR_OF_DAY);
-            int minute = c.get(Calendar.MINUTE);
+    @Override
+    public Dialog onCreateDialog(Bundle savedlnstanceState) {
+        //현재 시간을 picker의 기본 값으로 사용합니다.
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+        //새 인스턴스를 만들고 그것을 반환합니다.
+        return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
+    }
 
-            //새 인스턴스를 만들고 그것을 반환합니다.
-            return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
-        }
+    @Override
+    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+
     }
 }
